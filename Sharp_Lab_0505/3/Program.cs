fb33cd3275ca34ec7045087b0c6f0b5031348565
Viryz <viryz1998@gmail.com>
2018-05-22 01:09:03 +0300

    3 task

---
+++ Sharp_Lab_0505/3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter string: ");
+            string str = Console.ReadLine();
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] != str[str.Length - i - 1])
+                {
+                    Console.WriteLine("No");
+                    Console.ReadLine();
+                    return;
+                }
+            }
+            Console.WriteLine("Yes");
+            Console.ReadLine();
+        }
+    }
+}
