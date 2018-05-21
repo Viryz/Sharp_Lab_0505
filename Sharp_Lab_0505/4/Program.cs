c87cc7dad9680466f44995db7375ee98124f9046
Viryz <viryz1998@gmail.com>
2018-05-22 01:26:45 +0300

    4 task

---
+++ Sharp_Lab_0505/4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter string: ");
+            string str = Console.ReadLine();
+            Console.WriteLine("Count of words: {0}", str.Split(new[] { ' ', ',', '.' }, StringSplitOptions.RemoveEmptyEntries).Length);
+            Console.ReadLine();
+        }
+    }
+}
