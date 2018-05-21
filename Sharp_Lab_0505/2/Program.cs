f11362e0d27b95d6e69675062233fca01b4e7a89
Viryz <viryz1998@gmail.com>
2018-05-22 01:02:39 +0300

    2 task

---
+++ Sharp_Lab_0505/2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int N = 5, M = 5;
+            Random r = new Random();
+            int[] arr1 = new int[N];
+            int[] arr2 = new int[M];
+            for (int i = 0; i < N; i++)
+            {
+                Console.Write("Enter number for first array: ");
+                arr1[i] = int.Parse(Console.ReadLine());
+                Console.Write("Enter number for second array: ");
+                arr2[i] = int.Parse(Console.ReadLine());
+            }
+            IEnumerable<int> arr = arr1.Intersect(arr2);
+            foreach (int item in arr)
+                Console.Write(item.ToString() + " ");
+            Console.WriteLine();
+
+            Console.ReadLine();
+
+        }
+    }
+}
